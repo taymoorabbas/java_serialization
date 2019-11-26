c4089a22568ddb7faeb9f521d108675dde0b3b86
taymoorabbas <www.taymoor@gmail.com>
2019-11-26 21:04:59 +0500

    created serializable class

---
+++ src/Wrestler.java
@@ -0,0 +1,58 @@
+/*
+Created by: Taymoor Ghazanfar
+R.no: 3625-BSSE-F17-C
+Date: 26-Nov-19
+Time: 9:01 PM
+Lau ji Ghauri aya fir
+*/
+
+import java.io.Serializable;
+
+public class Wrestler implements Serializable {
+
+    private String name;
+    private char gender;
+    private String maneuver;
+    private int championships;
+
+    public Wrestler(){}
+
+    public Wrestler(String name, char gender, String maneuver, int championships) {
+        this.name = name;
+        this.gender = gender;
+        this.maneuver = maneuver;
+        this.championships = championships;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public String getManeuver() {
+        return maneuver;
+    }
+
+    public void setManeuver(String maneuver) {
+        this.maneuver = maneuver;
+    }
+
+    public int getChampionships() {
+        return championships;
+    }
+
+    public void setChampionships(int championships) {
+        this.championships = championships;
+    }
+}
